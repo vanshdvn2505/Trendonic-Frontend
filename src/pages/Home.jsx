7be58158b6c73fdfed3919f63f3e5b551b439609
Vansh Dhawan <152591281+vanshdvn2505@users.noreply.github.com>
2024-07-18 13:02:29 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,7 +58,7 @@ function Home() {
   const openProduct = (prod) => {
     try {
         dispatch(setOpenProd(prod));
-        const newWindow = window.open('https://trendonic-backend.onrender.com/open_product/' + prod._id, '_blank')
+        const newWindow = window.open('https://trendonic-frontend.onrender.com/open_product/' + prod._id, '_blank')
         if(newWindow){
             newWindow.focus();
         }
