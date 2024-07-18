b562746e2f66020030777aa4c31f86a663abd457
Vansh Dhawan <152591281+vanshdvn2505@users.noreply.github.com>
2024-07-18 13:02:45 +0530

    Update Open_Product.jsx

--- src/pages/Open_Product.jsx
+++ src/pages/Open_Product.jsx
@@ -74,7 +74,7 @@ function Open_Product() {
   const openProduct = (prod) => {
     try {
         dispatch(setOpenProd(prod));
-        const newWindow = window.open('https://trendonic-backend.onrender.com/open_product/' + prod._id, '_blank')
+        const newWindow = window.open('https://trendonic-frontend.onrender.com/open_product/' + prod._id, '_blank')
         if(newWindow){
             newWindow.focus();
         }
